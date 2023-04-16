812b3e629e2ee0b4a58c164cea96378347c40019
воробйов александр <avorobeov21@gmail.com>
2023-04-16 14:47:19 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            const int NumberToAdd1 = 3;
+            const int NumberToAdd2 = 5;
+            int maximumNumber = 101;
+            int minimalNumber = 0;
+            int randomNumber = random.Next(minimalNumber, maximumNumber);
+            int sumNumbers = 0;
+
+            for (int i = minimalNumber; i <= randomNumber; i++)
+            {
+                switch (i)
+                {
+                    case NumberToAdd1:
+                    case NumberToAdd2:
+                        sumNumbers += i;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Рандомно сгенерированное число: {randomNumber} \nСумма чисел: {sumNumbers}");
+            Console.ReadLine();
+        }
+    }
+}
